b048eaafee2234f71cc3f03d428a2e274d47b956
= <=>
2022-06-14 00:11:24 +0200

    letzte commit

--- main.js
+++ main.js
@@ -7,18 +7,45 @@ let i = 0;
 azalt.addEventListener("click", function () {
   i--;
   document.querySelector("#value").innerHTML = i;
-  if (i < 0) {value.style.color = "red";}
+  if (i < 0) {
+    value.style.color = "red";
+  }
 });
 
 artir.addEventListener("click", function () {
   i++;
   document.querySelector("#value").innerHTML = i;
-  if (i > 0) {value.style.color = "green";}
+  if (i > 0) {
+    value.style.color = "green";
+  }
 });
 
 reset.addEventListener("click", function () {
   i = 0;
   document.querySelector("#value").innerHTML = i;
 
-  if (i === 0) {value.style.color = "black";}
+  if (i === 0) {
+    value.style.color = "black";
+  }
 });
+
+
+
+const main = document.querySelector("main");
+const colors = [
+  "black",
+  "yellow",
+  "blue",
+  "green",
+  "orange",
+  "coral",
+  "cyan",
+  "hotpink",
+];
+
+let a = 0;
+setInterval(function () {
+  if (a == colors.length) a = 0;
+  main.style.background = colors[a];
+  a++;
+}, 1200);
